286334665234b595195ebbf6f058d375f4dde3c6
kirandevops262-spec <kirandevops262@gmail.com>
2026-02-23 14:09:28 +0530

    Delete EC2 IAM role and instance profile resources
    Removed the IAM role resource and instance profile for EC2.

--- module/vpc-ec2/iam.tf
+++ module/vpc-ec2/iam.tf
@@ -1,20 +1,3 @@
-resource "aws_iam_role" "iam-role" {
-  name = var.ec2-iam-role
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
 resource "aws_iam_role_policy" "iam-role-policy" {
   name = var.ec2-iam-role-policy
   role = aws_iam_role.iam-role.id
@@ -54,22 +37,17 @@ resource "aws_iam_role_policy" "iam-role-policy" {
         Resource = "*"
       },
       {
-        Effect: "Allow",
-        Action: [
-           "ec2messages:AcknowledgeMessage",
-           "ec2messages:DeleteMessage",
-           "ec2messages:FailMessage",
-           "ec2messages:GetEndpoint",
-           "ec2messages:GetMessages",
-           "ec2messages:SendReply"
-            ],
-        Resource: "*"
-        }
+        Effect   = "Allow"
+        Action   = [
+          "ec2messages:AcknowledgeMessage",
+          "ec2messages:DeleteMessage",
+          "ec2messages:FailMessage",
+          "ec2messages:GetEndpoint",
+          "ec2messages:GetMessages",
+          "ec2messages:SendReply"
+        ]
+        Resource = "*"
+      }
     ]
   })
-}
-
-resource "aws_iam_instance_profile" "ec2-instance-profile" {
-  name = var.ec2-iam-instance-profile
-  role = aws_iam_role.iam-role.name
 }

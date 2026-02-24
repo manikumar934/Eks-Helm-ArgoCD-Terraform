aedf393dd8947e6fad2000937712291b7c15ba40
tara <tarakiran2008@gmail.com>
2026-02-24 22:16:16 +0530

    Add Terraform state files and provider configurations for EKS and VPC-EC2 modules
    - Created Terraform state files for EKS and VPC-EC2 modules, specifying S3 backend configuration.
    - Added .terraform.lock.hcl files for AWS and Kubernetes providers with specific versions and hashes.
    - Introduced modules.json to manage module sources for VPC-EC2.
    - Included license files for AWS and Kubernetes providers.
    - Added provider binaries for AWS and Kubernetes for version 5.49.0 and 2.31.0 respectively.

--- module/vpc-ec2/ec2.tf
+++ module/vpc-ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   availability_zone      = "us-east-1a"
   iam_instance_profile   = aws_iam_instance_profile.ec2-instance-profile.id
   subnet_id              = aws_subnet.public-subnet[0].id
